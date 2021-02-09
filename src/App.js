fd79a62490afac7f8034dbdbcb94e6d5bb8c5761
JP Castrillon <jpcas001@gmail.com>
2021-02-09 17:56:34 -0500

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+          <Navigation/>
      <h1>Test</h1>
     </div>
   );
---
+++ src/components/Navigation.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Button, Nav, Navbar} from 'react-bootstrap';
+
+
+function Navigation() {
+    return (
+        <>
+      <img
+        src="https://i.imgur.com/DFjZCkS.png"
+        className="navbar-logo"
+        alt="scarablogo"
+      />
+    
+  <Nav className="justify-content-end">
+    <Nav.Item>
+      <Nav.Link href="/home">BIKES</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-1">JOURNAL</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-2">PROCESS</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-2">CONTACT</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Button>
+        order a bike
+      </Button>
+    </Nav.Item>
+  </Nav>
+</>
+    )
+}
+
+export default Navigation
