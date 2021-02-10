3c7dcefb0b427f4ba15289e7bf6e7df9c5acea45
JP Castrillon <jpcas001@gmail.com>
2021-02-10 12:20:36 -0500

    adding some css for the navbar and homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Navigation from './components/Navigation';
 import HomePage from './pages/HomePage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-          <Navigation/>
+          {/* <Navigation/> */}
           <HomePage />
 
     </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React from 'react';
+import {Card, Button, Nav} from 'react-bootstrap';
 
 function HomePage() {
     return (
-        <div className="home-background ">
-            <h1>This is the homepage</h1>
-        </div>
+ 
+        <Card className="home-background" >
+        <Card.Img src="https://i.imgur.com/HPBFCW7.jpg" alt="Card image" />
+        <Card.ImgOverlay>
+        <img
+        src="https://i.imgur.com/DFjZCkS.png"
+        className="navbar-logo"
+        alt="scarablogo"
+      />
+        <Nav className="justify-content-end">
+    <Nav.Item>
+      <Nav.Link href="/home" className="bikes">BIKES</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-1" className="bikes">JOURNAL</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-2" className="bikes">PROCESS</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-2"className="bikes">CONTACT</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Button>
+        order a bike
+      </Button>
+    </Nav.Item>
+  </Nav>
+          <Card.Title className="title1">Hi, I'm JP.</Card.Title>
+          <Card.Text className="description">
+            I'm a full stack web developer. 
+          </Card.Text>
+          <div className="button">
+       
+          <Button variant="light" size="lg" >Get in Touch</Button> {' '}
+          </div>
+        </Card.ImgOverlay>
+      
+      </Card>
+        
     )
 }
 
