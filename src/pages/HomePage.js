972040d6b23fd91b538cac9da9487704176f4611
JP Castrillon <jpcas001@gmail.com>
2021-02-11 12:22:09 -0500

    updating homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import {Nav} from 'react-bootstrap';
+import {Nav, Button, Card} from 'react-bootstrap';
 import '../App.css';
 
 function HomePage() {
     return (
-<>
+<Card className="background-home">
+<Card.Img src='https://i.imgur.com/CWeKPSu.jpeg' alt="Card image" />
+<Card.ImgOverlay>
+<Nav className="navbar-logo">
+<img
+        src="https://i.imgur.com/DFjZCkS.png"
+        className="navbar-logo"
+        alt="scarablogo"
+        ></img>
+</Nav>
 
     <Nav className="justify-content-end">
     <Nav.Item >
@@ -17,12 +26,15 @@ function HomePage() {
       <Nav.Link eventKey="link-2" className="bikes">PROCESS</Nav.Link>
     </Nav.Item>
     <Nav.Item>
-      <Nav.Link eventKey="disabled" disabled>
-        Disabled
-      </Nav.Link>
+      <Nav.Link eventKey="link-2" className="bikes">CONTACT</Nav.Link>
     </Nav.Item>
+    <Button className="order-button">
+        order a bike
+      </Button>
+      
   </Nav>
-</>
+  </Card.ImgOverlay>
+</Card>
         
         
     )
